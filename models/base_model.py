4a8fa93f150600359dde3dfd55792c499d07895d
Kingzbrian <murageb254@gmail.com>
2023-02-12 03:02:36 -0800

    "base model"

---
+++ models/base_model.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+"""This module defines a base class for all models in our hbnb clone"""i
+import uuid
+from datetime import datetime
+import models
+
+class BaseModel:
+     """
+        A base class for all hbnb models
+        Attributes:
+            id (siqlalchemy String): The BaseModel id.
+            created_at (sqlalchemy DateTime): The datetime at creation.
+            updated_at (sqlalchemy DateTime): The datetime of last update.
+    """
+    def __init__(self, *args, **kwargs):
+        """
+            initialize public instance attributes
+        """
+        if not kwargs:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            models.storage.new(self)
+
+        else:
+            kwargs["created_at"] = datetime.strptime(kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
+             kwargs["updated_at"] = datetime.strptime(kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
+
+             for key, val in kwargs.items():
+                 if "__class__" not in key:
+                     setattr(self, key, val)
+
+    def __str__(self):
+        '''
+            Return string representation of BaseModel class
+        '''
+        return ("[{}]i ({}) {}".format(self.__class__.__name__,self.id, self.__dict__))
+
+    def save():
+         self.updated_at = datetime.now()
+         models.storage.save()
+
+    def to_dict(self):
+        '''
+            Return dictionary representation of BaseModel class.
+        '''
+        dct = dict(self.__dict__)
+        dct['__class__'] = self.__class__.__name__
+        dct['created_at'] = self.created_at.strftime("%Y-%m-%dT%H:%M:%S.%f")
+        dct['updated_at'] = self.updated_at.strftime("%Y-%m-%dT%H:%M:%S.%f")
+        return(dct)
